b0334881c6de2515c1157996a30c7539a9b25416
whitehat92 <ze_correia93@hotmail.com>
2019-09-12 16:37:50 +0100

    Create simplehttpsprobe.py

---
+++ simplehttpsprobe.py
@@ -0,0 +1,14 @@
+import requests
+import sys
+
+headers = {'User-agent':'Mozilla//5.0',}
+
+url = sys.argv[1]
+addhttps = "https://"
+if not "https://" in url:
+        url = addhttps + url
+pedido = requests.get(url, headers=headers)
+estado = pedido.status_code
+
+print(estado)
+print(url)
